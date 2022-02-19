4748d9c79f09eeb4c4d1d84d298eebc19bd497c3
Yago Pajariño <yago977@hotmail.com>
2022-02-19 20:10:30 -0300

    adds minor changes

--- src/scripts.js
+++ src/scripts.js
@@ -3,3 +3,11 @@ import './style.css';
 const Country = require('country-list');
 
 const countries = Country.getNames();
+const selector = document.querySelector('#countrySelector');
+
+countries.forEach((country) => {
+  const option = document.createElement('option');
+  option.value = country;
+  option.textContent = country;
+  selector.appendChild(option);
+});
