6716f35f612c2e0a252e35fee7ed830ccc4c0f75
Yago Pajariño <yago977@hotmail.com>
2022-02-21 12:47:41 -0300

    adds icon

--- main.js
+++ main.js
@@ -50,6 +50,16 @@ eval("\n\nmodule.exports = function (i) {\n  return i[1];\n};\n\n//# sourceURL=w
 
 /***/ }),
 
+/***/ "./src/icon.png":
+/*!**********************!*\
+  !*** ./src/icon.png ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"527ccfe16a2dbfc90af986c8137a07bd.png\");\n\n//# sourceURL=webpack://top-formvalidation/./src/icon.png?");
+
+/***/ }),
+
 /***/ "./src/style.css":
 /*!***********************!*\
   !*** ./src/style.css ***!
@@ -126,7 +136,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nconst body = document.querySelector('body');\nconst Country = __webpack_require__(/*! country-list */ \"./node_modules/country-list/country-list.js\");\n\nconst countries = Country.getNames();\nconst selector = document.querySelector('#countrySelector');\n\n// Country selector list\ncountries.forEach((country) => {\n  const option = document.createElement('option');\n  option.value = country;\n  option.textContent = country;\n  selector.appendChild(option);\n});\n\n// Form validations\nconst email = document.querySelector('#email');\nconst emailError = document.querySelector('#email + span');\n\nconst zip = document.querySelector('#zip');\nconst zipError = document.querySelector('#zip + span');\n\nconst country = document.querySelector('#countrySelector');\nconst countryError = document.querySelector('#countrySelector + span');\n\nconst password = document.querySelector('#pwd');\nconst pwdConfirm = document.querySelector('#pwdConf');\nconst pwdError = document.querySelector('#pwdConf + span');\n\nconst hey = document.createElement('h1');\nhey.textContent = 'Great! all fields are OK.';\n\n// Show errors functions\nfunction showEmailError() {\n  if (email.validity.valueMissing) {\n    emailError.textContent = 'You need to enter an e-mail address.';\n  } else if (email.validity.typeMismatch) {\n    emailError.textContent = 'Please enter a valid email';\n  }\n  emailError.className = 'error';\n}\n\nfunction showZipError() {\n  if (zip.validity.valueMissing) {\n    zipError.textContent = 'Enter your zip code';\n    zipError.classList = 'error';\n  }\n}\n\nfunction showCountryError() {\n  if (country.value == 'Select country') {\n    countryError.textContent = 'Select a country from the list.';\n  }\n  countryError.classList = 'error';\n}\n\nfunction showPwdError() {\n  if (pwdConfirm.validity.valueMissing) {\n    pwdError.textContent = 'Please confirm the password';\n  } else if (password.value != pwdConfirm.value) {\n    pwdError.textContent = 'Passwords not matching';\n  }\n  pwdError.classList = 'error';\n}\n\n// Fields event Listeners\npwdConfirm.addEventListener('change', () => {\n  let c = true;\n  if (password.value != pwdConfirm.value) {\n    c = false;\n  }\n  if (c) {\n    pwdError.classList = '';\n    pwdError.textContent = '';\n    pwdConfirm.classList = 'valid';\n    password.classList = 'valid';\n  } else {\n    showPwdError();\n  }\n});\n\nzip.addEventListener('change', () => {\n  if (zip.validity.valid) {\n    zipError.textContent = '';\n    zipError.classList = '';\n    zip.classList = 'valid';\n  } else {\n    showZipError();\n  }\n});\n\ncountry.addEventListener('change', () => {\n  if (country.validity.valid) {\n    countryError.textContent = '';\n    countryError.classList = '';\n    country.classList = 'valid';\n  } else {\n    showCountryError();\n  }\n});\n\nemail.addEventListener('change', () => {\n  if (email.validity.valid) {\n    emailError.textContent = '';\n    emailError.classList = '';\n    email.classList = 'valid';\n  } else {\n    showEmailError();\n  }\n});\n\nconst form = document.querySelector('form');\nconst button = document.querySelector('button');\nbutton.addEventListener('click', (event) => {\n  if (!email.validity.valid) {\n    showEmailError();\n    event.preventDefault();\n  }\n  if (country.value == 'Select country') {\n    showCountryError();\n    event.preventDefault();\n  }\n  if (!zip.validity.valid) {\n    showZipError();\n    event.preventDefault();\n  }\n  if (!pwdConfirm.validity.valid) {\n    showPwdError();\n    event.preventDefault();\n  } else {\n    form.classList = 'hidden';\n    body.appendChild(hey);\n  }\n});\n\n\n//# sourceURL=webpack://top-formvalidation/./src/scripts.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _icon_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./icon.png */ \"./src/icon.png\");\n/* eslint-disable eqeqeq */\n\n\n\nconst body = document.querySelector('body');\nconst Country = __webpack_require__(/*! country-list */ \"./node_modules/country-list/country-list.js\");\n\nconst countries = Country.getNames();\nconst selector = document.querySelector('#countrySelector');\n\n// Country selector list\ncountries.forEach((country) => {\n  const option = document.createElement('option');\n  option.value = country;\n  option.textContent = country;\n  selector.appendChild(option);\n});\n\n// Form validations\nconst email = document.querySelector('#email');\nconst emailError = document.querySelector('#email + span');\n\nconst zip = document.querySelector('#zip');\nconst zipError = document.querySelector('#zip + span');\n\nconst country = document.querySelector('#countrySelector');\nconst countryError = document.querySelector('#countrySelector + span');\n\nconst password = document.querySelector('#pwd');\nconst pwdConfirm = document.querySelector('#pwdConf');\nconst pwdError = document.querySelector('#pwdConf + span');\n\nconst hey = document.createElement('h1');\nhey.textContent = 'Great! all fields are OK.';\n\n// Show errors functions\nfunction showEmailError() {\n  if (email.validity.valueMissing) {\n    emailError.textContent = 'You need to enter an e-mail address.';\n  } else if (email.validity.typeMismatch) {\n    emailError.textContent = 'Please enter a valid email';\n  }\n  emailError.className = 'error';\n}\n\nfunction showZipError() {\n  if (zip.validity.valueMissing) {\n    zipError.textContent = 'Enter your zip code';\n    zipError.classList = 'error';\n  }\n}\n\nfunction showCountryError() {\n  if (country.value == 'Select country') {\n    countryError.textContent = 'Select a country from the list.';\n  }\n  countryError.classList = 'error';\n}\n\nfunction showPwdError() {\n  if (pwdConfirm.validity.valueMissing) {\n    pwdError.textContent = 'Please confirm the password';\n  } else if (password.value != pwdConfirm.value) {\n    pwdError.textContent = 'Passwords not matching';\n  }\n  pwdError.classList = 'error';\n}\n\n// Fields event Listeners\npwdConfirm.addEventListener('change', () => {\n  let c = true;\n  if (password.value != pwdConfirm.value) {\n    c = false;\n  }\n  if (c) {\n    pwdError.classList = '';\n    pwdError.textContent = '';\n    pwdConfirm.classList = 'valid';\n    password.classList = 'valid';\n  } else {\n    showPwdError();\n  }\n});\n\nzip.addEventListener('change', () => {\n  if (zip.validity.valid) {\n    zipError.textContent = '';\n    zipError.classList = '';\n    zip.classList = 'valid';\n  } else {\n    showZipError();\n  }\n});\n\ncountry.addEventListener('change', () => {\n  if (country.validity.valid) {\n    countryError.textContent = '';\n    countryError.classList = '';\n    country.classList = 'valid';\n  } else {\n    showCountryError();\n  }\n});\n\nemail.addEventListener('change', () => {\n  if (email.validity.valid) {\n    emailError.textContent = '';\n    emailError.classList = '';\n    email.classList = 'valid';\n  } else {\n    showEmailError();\n  }\n});\n\nconst form = document.querySelector('form');\nconst button = document.querySelector('button');\nbutton.addEventListener('click', (event) => {\n  if (!email.validity.valid) {\n    showEmailError();\n    event.preventDefault();\n  }\n  if (country.value == 'Select country') {\n    showCountryError();\n    event.preventDefault();\n  }\n  if (!zip.validity.valid) {\n    showZipError();\n    event.preventDefault();\n  }\n  if (!pwdConfirm.validity.valid) {\n    showPwdError();\n    event.preventDefault();\n  } else {\n    form.classList = 'hidden';\n    body.appendChild(hey);\n  }\n});\n\n\n//# sourceURL=webpack://top-formvalidation/./src/scripts.js?");
 
 /***/ }),
 
@@ -191,6 +201,18 @@ eval("module.exports = JSON.parse('[{\"code\":\"AD\",\"name\":\"Andorra\"},{\"co
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -205,6 +227,26 @@ eval("module.exports = JSON.parse('[{\"code\":\"AD\",\"name\":\"Andorra\"},{\"co
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
